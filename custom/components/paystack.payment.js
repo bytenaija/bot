207a008d9cde9aa7f0e2617270cabb74de726b26
Everistus Olumese <everistusolumese@gmail.com>
2018-12-13 01:37:52 +0100

    Aiico

--- custom/components/paystack.payment.js
+++ custom/components/paystack.payment.js
@@ -29,7 +29,7 @@ module.exports = {
   }),
   invoke: (conversation, done) => {
     //     // perform conversation tasks.
-   const { cardDetails, email, amount } = conversation.properties();
+   const { cardDetails, email, amount, transactionRef } = conversation.properties();
 
    axios.defaults.headers.post['Authorization'] = 'Bearer sk_test_f4a095ef53406f3f9488ab67d7f9e67e046ca8dd';
 
@@ -41,7 +41,7 @@ module.exports = {
       "Content-Type": "application/json"
     }
     const card = {
-      number: cardDetails.number,
+      number: cardDetails.cardNumber,
       cvv: cardDetails.cvv,
       expiry_month: expiry[0],
       expiry_year: expiry[1]
