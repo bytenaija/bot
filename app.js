0a390d01406fd9f86dae67dde15c74ab5d9fa84e
Everistus Olumese <everistusolumese@gmail.com>
2018-12-18 12:25:54 +0100

    password reset pug

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const axios = require('axios')
 const EmailService = require('./custom/services/EmailService');
 var generator = require('generate-password');
  
@@ -9,4 +10,35 @@ var password = generator.generate({
     excludeSimilarCharacters:true
 });
 
-EmailService.email("everistusolumese@gmail.com", password)
+// EmailService.email("everistusolumese@gmail.com", password)
+axios.defaults.headers.post['Authorization'] = 'Bearer sk_test_dce12f10f109e0a79d04e8f1615610e9d89c240e';
+
+const headers = {
+    "Authorization": 'Bearer sk_test_dce12f10f109e0a79d04e8f1615610e9d89c240e',
+    "Content-Type": "application/json"
+  }
+  const card = {
+    number: '408 408 408 408 408 1',
+    cvv: '408',
+    expiry_month: '10',
+    expiry_year: '21'
+  }
+
+  const transaction = {
+    email: 'everistusolumese@gmail.com',
+    amount: 250000 * 100,
+    reference: 'TRS-Goat-Payment-' + Date.now(),
+    card
+  }
+
+
+  axios.post(`https://api.paystack.co/charge`, 
+      transaction
+    )
+    .then(chargeResponse => {
+      console.log(chargeResponse.data)
+      // Handle the charge response
+      if (chargeResponse.data.status) {
+       
+      }
+    }).catch(err => console.log(err))
