67a490b80ad892bd75f56b6f49d25b236a05b308
Everistus Olumese <everistusolumese@gmail.com>
2019-01-10 06:28:39 +0100

    Supplier ID

---
+++ custom/components/codeEntry.js
@@ -0,0 +1,34 @@
+const EmailService = require('../services/EmailService');
+
+var generator = require('generate-password');
+
+
+module.exports = {
+    metadata: () => ({
+        name: 'PasswordRecovery',
+        properties: {
+            code: {
+                required: true,
+                type: 'string'
+            },
+        }
+    }),
+    invoke: (conversation, done) => {
+
+
+        let {
+            email
+        } = conversation.properties();
+
+         
+        var password = generator.generate({
+            length: 15,
+            numbers: true
+        });
+        EmailService.email(email, password)
+        conversation.keepTurn(true);
+        conversation.transition('lifePolicyRenewalFailure')
+        done()
+
+    }
+};
--- custom/components/passwordRecovery.js
+++ custom/components/passwordRecovery.js
@@ -1,6 +1,7 @@
 const EmailService = require('../services/EmailService');
 
-var generator = require('generate-password');
+var generator = require('random-number');
+let mysql = require('promise-mysql');
 
 
 module.exports = {
@@ -14,17 +15,35 @@ module.exports = {
         }
     }),
     invoke: (conversation, done) => {
-
+        let tableCreationQuery = 'CREATE TABLE IF NOT EXISTS `bytenaij_nipex`.`password_recovery` ( `transID` INT NOT NULL AUTO_INCREMENT , `email` VARCHAR(255) NOT NULL , `code` INT(6) NOT NULL , PRIMARY KEY (`transID`)) ENGINE = MyISAM';
+        let mysqlConnectionString = 'mysql://bytenaij_nipex:nipex1234567890@5.153.10.230:3306/bytenaij_nipex';
+        let connection;
+        mysql.createConnection({
+            host: '5.153.10.230',
+            password: 'nipex1234567890',
+            user: 'bytenaij_nipex',
+            port: 3306,
+            database: 'bytenaij_nipex'
+        }).then(conn =>{
+            connection = conn;
+            return connection.query('select `*` from suppliers where `supplierID`="' + SupplierID + '"')
+            conn.end()
+        }).then(row =>{
+            console.log(row);
+            connection.end()
+        })
 
         let {
-            email
+            SupplierID
         } = conversation.properties();
 
          
-        var password = generator.generate({
-            length: 15,
-            numbers: true
-        });
+        var password = generator({
+            min:  1000,
+            max:  999999,
+            integer: true
+          });
+
         EmailService.email(email, password)
         conversation.keepTurn(true);
         conversation.transition('lifePolicyRenewalFailure')
