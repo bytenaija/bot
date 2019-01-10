073e38df434ae1ac06d870367f5677f6fbd6ada0
Everistus Olumese <everistusolumese@gmail.com>
2019-01-10 06:37:31 +0100

    Supplier ID

--- custom/components/passwordRecovery.js
+++ custom/components/passwordRecovery.js
@@ -17,6 +17,12 @@ module.exports = {
     invoke: (conversation, done) => {
         let tableCreationQuery = 'CREATE TABLE IF NOT EXISTS `bytenaij_nipex`.`password_recovery` ( `transID` INT NOT NULL AUTO_INCREMENT , `email` VARCHAR(255) NOT NULL , `code` INT(6) NOT NULL , PRIMARY KEY (`transID`)) ENGINE = MyISAM';
         let mysqlConnectionString = 'mysql://bytenaij_nipex:nipex1234567890@5.153.10.230:3306/bytenaij_nipex';
+        
+        let {
+            SupplierID
+        } = conversation.properties();
+
+        console.log("Supplier ID", SupplierID)
         let connection;
         mysql.createConnection({
             host: '5.153.10.230',
@@ -26,28 +32,27 @@ module.exports = {
             database: 'bytenaij_nipex'
         }).then(conn =>{
             connection = conn;
-            return connection.query('select `*` from suppliers where `supplierID`="' + SupplierID + '"')
+            return connection.query('select * from suppliers where `supplierID`="' + SupplierID + '"')
             conn.end()
         }).then(row =>{
             console.log(row);
             connection.end()
-        })
+            // EmailService.email(email, password)
 
-        let {
-            SupplierID
-        } = conversation.properties();
-
-         
+                  
         var password = generator({
             min:  1000,
             max:  999999,
             integer: true
           });
+          console.log("Pass code", password)
+            conversation.keepTurn(true);
+            conversation.transition()
+            done()
+            
+        })
 
-        EmailService.email(email, password)
-        conversation.keepTurn(true);
-        conversation.transition('lifePolicyRenewalFailure')
-        done()
+   
 
     }
 };
