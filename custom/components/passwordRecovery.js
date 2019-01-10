6522bf9de942d36f4d250278b5db561a8a80015d
Everistus Olumese <everistusolumese@gmail.com>
2019-01-10 06:56:33 +0100

    Supplier ID

--- custom/components/passwordRecovery.js
+++ custom/components/passwordRecovery.js
@@ -12,7 +12,8 @@ module.exports = {
                 required: true,
                 type: 'string'
             },
-        }
+        },
+        supportedActions: ['WrongID']
     }),
     invoke: (conversation, done) => {
         let tableCreationQuery = 'CREATE TABLE IF NOT EXISTS `bytenaij_nipex`.`password_recovery` ( `transID` INT NOT NULL AUTO_INCREMENT , `email` VARCHAR(255) NOT NULL , `code` INT(6) NOT NULL , PRIMARY KEY (`transID`)) ENGINE = MyISAM';
@@ -35,21 +36,29 @@ module.exports = {
             return connection.query('select * from suppliers where `supplierID`="' + SupplierID + '"')
             conn.end()
         }).then(row =>{
-            let {email, name, SupplierID} = row[0];
+            if(row.length != 0){
+                let {email, name, supplierID} = row[0];
 
-            console.log(email, name, SupplierID)
-            connection.end()
-            // EmailService.email(email, password)
+                console.log(email, name, supplierID)
+                let result =  connection.query(tableCreationQuery);
 
-                  
-        var password = generator({
-            min:  1000,
-            max:  999999,
-            integer: true
-          });
-          console.log("Pass code", password)
-            conversation.keepTurn(true);
-            conversation.transition()
+                console.log(result)
+                connection.end()
+                EmailService.email(email, password)
+    
+                      
+            var password = generator({
+                min:  1000,
+                max:  999999,
+                integer: true
+              });
+              console.log("Pass code", password)
+                conversation.keepTurn(true);
+                conversation.transition()
+            }else{
+                conversation.transition('WrongID')
+            }
+
             done()
             
         })
