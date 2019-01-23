9de80bdea7b4b4ecb8aa205f07eabe8672cc5967
Everistus Olumese <everistusolumese@gmail.com>
2019-01-23 15:09:52 +0100

    Plane Crash

--- custom/components/ComputeTravelPremium.js
+++ custom/components/ComputeTravelPremium.js
@@ -1,16 +1,32 @@
 const axios = require('axios');
+const moment = require('moment')
 
 module.exports = {
     metadata: () => ({
         name: 'ComputeTravelPremium',
         properties: {
-            
+            countryId: {type: 'string', required: true},
+            dateOfBirth: {type: 'string', required: true},
+            startDate: {type: 'string', required: true},
+            endDate: {type: 'string', required: true}
         },
         supportedActions: ['travelInsuranceNotDefined']
     }),
     invoke: (conversation, done) => {
 
-       
+        let {countryId,
+            dateOfBirth,
+            startDate,
+            endDate} = conversation.properties()
+
+            dateOfBirth = moment(dateOfBirth).format('MM DD YYYY');
+            console.log("Date of birth", dateOfBirth)
+       let data = {
+            "destinationCountryId": countryId,
+        "dateOfBirth": dateOfBirth,
+        "startDate": moment(startDate).toISOString(),
+        "endDate": moment(endDate).toISOString()
+        }
         const headers = {
             "X-ApiKey": 'Pr4d++7WTRIzkzZHunc4+dyh6wWDmUBrj57AIhUXY6dG7TeZPFwwIvBW+ZBo8oK/',
             "Content-Type": "application/json"
@@ -18,9 +34,7 @@ module.exports = {
 
         axios.defaults.headers.post['X-ApiKey'] = 'Pr4d++7WTRIzkzZHunc4+dyh6wWDmUBrj57AIhUXY6dG7TeZPFwwIvBW+ZBo8oK/';
 
-        let colorList = []
-
-        axios.get(`https://e-business.aiicoplc.com:89/api/services/app/BuyProduct/ComputeTravelPremium`, {
+        axios.post(`https://e-business.aiicoplc.com:89/api/services/app/BuyProduct/ComputeTravelPremium`, data, {
                 headers: headers,
             })
             .then(response => {
@@ -30,26 +44,23 @@ module.exports = {
                     let {
                         result
                     } = response.data
-                    
-               
 
-                conversation.variable("colorList", colorList);
-                conversation.variable("yrManf", yrManf);
-                conversation.keepTurn(true)
-                conversation.transition()
-                done()
-       
-                  
+
+                    conversation.keepTurn(true)
+                    conversation.transition()
+                    done()
+
+
                 } else {
                     let msg = response.data.error.message
                     conversation.reply(msg)
-                    .transition('travelInsuranceNotDefined')
+                        .transition('travelInsuranceNotDefined')
                     done()
                 }
             }).catch(err => {
                 console.log("error", err);
                 let msg = err.error.message
-                    conversation.reply(msg)
+                conversation.reply(msg)
                     .transition('travelInsuranceNotDefined')
                 conversation.keepTurn(true);
                 conversation.transition('travelInsuranceNotDefined')
