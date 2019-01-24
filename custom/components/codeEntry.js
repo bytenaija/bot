6cd08a731af973fdd398dc4ac9dbbc14ed2a16d1
Everistus Olumese <everistusolumese@gmail.com>
2019-01-24 13:46:37 +0100

    ComputeTravelPremium

--- custom/components/codeEntry.js
+++ custom/components/codeEntry.js
@@ -22,6 +22,7 @@ module.exports = {
         } = conversation.properties();
         
         let connection;
+        if(SystemType == 'National Joint Qualification System (NJQS) (Qualification system)'){
         mysql.createConnection({
             host: 'test.nipexjqs.com',
             password: 'softalliance',
@@ -45,8 +46,8 @@ module.exports = {
                     excludeSimilarCharacters: true,
                 });
                 connection.query('DELETE FROM `password_recovery` WHERE `code` = "' + code + '"').then(result => {
-                    connection.query("UPDATE `sec_supp_users` SET `pswd` = '" + password + "' WHERE `sec_supp_users`.`email` = '" + email + "'").then(result => {
-                        return connection.query('select * from sec_supp_users where `email`="' + email + '"')
+                    connection.query("UPDATE `tbl_vendor_mst` SET `pswd` = '" + password + "' WHERE `tbl_vendor_mst`.`email` = '" + email + "'").then(result => {
+                        return connection.query('select * from tbl_vendor_mst where `email`="' + email + '"')
 
                     }).then(row => {
                         if (row.length != 0) {
@@ -90,5 +91,103 @@ module.exports = {
 
 
 
+    }else if(SystemType == 'Vendor Registration (Pre-Qualification) system (VRS)'){ 
+
+        mysql.createConnection({
+            host: '80.241.219.166',
+            password: 'N1p2e3x4#',
+            user: 'nipex_staging',
+            port: 3306,
+            database: 'db_nipex_dnb'
+        }).then(conn => {
+            connection = conn;
+            return connection.query('select * from password_recovery where `code`="' + code + '"')
+
+        }).then(row => {
+            if (row.length != 0) {
+                let {
+                    email,
+                    QuestionnaireType
+                } = row[0];
+
+                var password = generator.generate({
+                    length: 15,
+                    numbers: true,
+                    uppercase: true,
+                    excludeSimilarCharacters: true,
+                });
+                connection.query('DELETE FROM `password_recovery` WHERE `code` = "' + code + '"').then(result => {
+                
+                    if(QuestionnaireType ==  'Pre-Questionnaire'){
+                        connection.query("UPDATE `tbl_company_mst` SET `fldv_company_password` = '" + password + "' WHERE `tbl_company_mst`.`fldv_email_id` = '" + email + "'").then(result => {
+                            return connection.query('select * from tbl_company_mst where `fldv_email_id`="' + email + '"')
+    
+                        }).then(row => {
+                            if (row.length != 0) {
+                                let {
+                                    email,
+                                    
+                                } = row[0];
+                                connection.end()
+                                EmailService.email(email, password, '', 'PasswordRecovery')
+                                conversation.keepTurn(true);
+                                conversation.transition()
+                                done()
+                            } else {
+                                connection.end()
+                                conversation.transition('codeEntryError')
+                            }
+                        }).catch(err => {
+                            connection.end()
+                            console.log(err);
+                            conversation.transition('codeEntryError')
+                            done()
+                        })
+                    }else{
+                        connection.query("UPDATE `tbl_vendor_mst` SET `fldv_password` = '" + password + "' WHERE `tbl_vendor_mst`.`fldv_issued_ID` = '" + email + "'").then(result => {
+                            return connection.query('select * from tbl_vendor_mst where `email`="' + email + '"')
+    
+                        }).then(row => {
+                            if (row.length != 0) {
+                                let {
+                                    email,
+                                    
+                                } = row[0];
+                                connection.end()
+                                EmailService.email(email, password, '', 'PasswordRecovery')
+                                conversation.keepTurn(true);
+                                conversation.transition()
+                                done()
+                            } else {
+                                connection.end()
+                                conversation.transition('codeEntryError')
+                            }
+                        }).catch(err => {
+                            connection.end()
+                            console.log(err);
+                            conversation.transition('codeEntryError')
+                            done()
+                        })
+                    }
+                    
+                })
+
+
+
+
+            } else {
+                connection.end()
+                conversation.transition('codeEntryError')
+                done()
+            }
+
+
+
+        }).catch(err => {
+            console.log(err);
+
+            connection.end()
+        })
     }
+}
 };
--- custom/components/passwordRecovery.js
+++ custom/components/passwordRecovery.js
@@ -15,12 +15,16 @@ module.exports = {
             SystemType: {
                 required: true,
                 type: 'string'
-            }
+            },
+            QuestionnaireType: {
+                required: false,
+                type: 'string'
+            },
         },
         supportedActions: ['WrongID', 'PasswordRecoveryError']
     }),
     invoke: (conversation, done) => {
-        let tableCreationQuery = 'CREATE TABLE IF NOT EXISTS `password_recovery` ( `transID` INT NOT NULL AUTO_INCREMENT , `email` VARCHAR(255) NOT NULL , `SystemType` VARCHAR(255) NOT NULL , `code` INT(6) NOT NULL , PRIMARY KEY (`transID`)) ENGINE = MyISAM';
+        let tableCreationQuery = 'CREATE TABLE IF NOT EXISTS `password_recovery` ( `transID` INT NOT NULL AUTO_INCREMENT , `email` VARCHAR(255) NOT NULL , `SystemType` VARCHAR(255) NOT NULL , `code` INT(6) NOT NULL , `level` VARCHAR(255) NULL , PRIMARY KEY (`transID`)) ENGINE = MyISAM';
       
 
         let {
@@ -98,13 +102,14 @@ module.exports = {
         })
 
      }else if(SystemType == 'Vendor Registration (Pre-Qualification) system (VRS)'){
+         let {QuestionnaireType} = conversation.properties();
             // 80.241.219.166
             // db db_nipex_dnb
             // user nipex_staging
             //  pass N1p2e3x4#
             // fldv_email_id
             // fldv_company_password
-
+        
             let connection;
         mysql.createConnection({
             host: '80.241.219.166',
@@ -114,54 +119,97 @@ module.exports = {
             database: 'db_nipex_dnb'
         }).then(conn => {
             connection = conn;
-           
-                return connection.query('select * from tbl_vendor_mst where `fldv_issue_ID`="' + SupplierID + '"')
+           if(QuestionnaireType == 'Pre-Questionnaire'){
+            return connection.query('select * from tbl_company_mst where `fldv_email_id`="' + SupplierID + '"')
+           }else{
+            return connection.query('select * from tbl_vendor_mst where `fldv_issue_ID`="' + SupplierID + '"')
+           }
+               
            
             
             
         }).then(row => {
             if (row.length != 0) {
-                let {
-                    fldv_email,
-                } = row[0];
-
-              
-                connection.query(tableCreationQuery).then(result => {
-                    var password = generator({
-                        min: 1000,
-                        max: 999999,
-                        integer: true
-                    });
-                    connection.query('DELETE FROM `password_recovery` WHERE `email` = "' + email + '"').then(result =>{
-                        connection.query("INSERT INTO `password_recovery` (`transID`, `email`, `SystemType`, `code`) VALUES (NULL, '" + email + "', '" + SystemType + "', '" + password + "')").then(result => {
-                            connection.end()
-                            EmailService.email(email, password, '', 'CodeGeneration')
-                            conversation.keepTurn(true);
-                            conversation.transition()
-                            done()
-                        }).catch(err =>{
-                            connection.end()
-                            console.log(err);
-                            conversation.transition('PasswordRecoveryError')
-                            done()
+                if(QuestionnaireType == 'Pre-Questionnaire'){
+                    let {
+                        fldv_email_id,
+                    } = row[0];
+    
+                  
+                    connection.query(tableCreationQuery).then(result => {
+                        var password = generator({
+                            min: 1000,
+                            max: 999999,
+                            integer: true
+                        });
+                        connection.query('DELETE FROM `password_recovery` WHERE `email` = "' + fldv_email_id + '"').then(result =>{
+                            connection.query("INSERT INTO `password_recovery` (`transID`, `email`, `SystemType`, `code`, `level) VALUES (NULL, '" + fldv_email_id + "', '" + SystemType + "', '" + password + "', '" + QuestionnaireType + "')").then(result => {
+                                connection.end()
+                                EmailService.email(email, password, '', 'CodeGeneration')
+                                conversation.keepTurn(true);
+                                conversation.transition()
+                                done()
+                            }).catch(err =>{
+                                connection.end()
+                                console.log(err);
+                                conversation.transition('PasswordRecoveryError')
+                                done()
+                            })
                         })
-                    })
-                    
-                }).catch(err =>{
+                        
+                    }).catch(err =>{
+                        connection.end()
+                        console.log(err);
+                        conversation.transition('PasswordRecoveryError');
+                        done();
+                    });
+    
+    
+                }else{
+
+                    let {
+                        fldv_email,
+                    } = row[0];
+    
+                  
+                    connection.query(tableCreationQuery).then(result => {
+                        var password = generator({
+                            min: 1000,
+                            max: 999999,
+                            integer: true
+                        });
+                        connection.query('DELETE FROM `password_recovery` WHERE `email` = "' + fldv_email + '"').then(result =>{
+                            connection.query("INSERT INTO `password_recovery` (`transID`, `email`, `SystemType`, `code`) VALUES (NULL, '" + fldv_email + "', '" + SystemType + "', '" + password + "', '" + QuestionnaireType + "')").then(result => {
+                                connection.end()
+                                EmailService.email(email, password, '', 'CodeGeneration')
+                                conversation.keepTurn(true);
+                                conversation.transition()
+                                done()
+                            }).catch(err =>{
+                                connection.end()
+                                console.log(err);
+                                conversation.transition('PasswordRecoveryError')
+                                done()
+                            })
+                        })
+                        
+                    }).catch(err =>{
+                        connection.end()
+                        console.log(err);
+                        conversation.transition('PasswordRecoveryError');
+                        done();
+                    });
+    
+
+                } 
+    
+             
+                }else {
                     connection.end()
-                    console.log(err);
-                    conversation.transition('PasswordRecoveryError');
-                    done();
-                });
-
-
-            } else {
-                connection.end()
-                conversation.transition('WrongID')
-                done()
-            }
-
-         
+                    conversation.transition('WrongID')
+                    done()
+                }
+               
             
         }).catch(err =>{
             console.log(err);
