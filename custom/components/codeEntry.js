aa90467b7915069885cc844a0950bb7f1ed1019f
Everistus Olumese <everistusolumese@gmail.com>
2019-02-09 10:04:28 +0100

    System Type

--- custom/components/codeEntry.js
+++ custom/components/codeEntry.js
@@ -46,7 +46,7 @@ module.exports = {
                     return false;
                 }
             }).catch(e =>{
-                console.log("THIS ERRORORORORORORORRO:",...e);
+                console.log("THIS ERRORORORORORORORRO:",e);
                 conn.end();
                 return false;
             }),
@@ -70,7 +70,7 @@ module.exports = {
                     return false;
                 }
             }).catch(e =>{
-                console.log("THIS ERRORORORORORORORRO:",...e);
+                console.log("THIS ERRORORORORORORORRO:",e);
                 
                 conn.end();
                 return false;
