3d004dd5c57ee67f1ab3be350c65e002f5cfda3a
Everistus Olumese <everistusolumese@gmail.com>
2019-02-11 05:52:30 +0100

    Malformed error

--- custom/components/doPostTravelInsuranceSchedule.js
+++ custom/components/doPostTravelInsuranceSchedule.js
@@ -148,6 +148,7 @@ module.exports = {
 
     } = conversation.properties();
     premium = parseFloat(premium);
+    sumAssured = parseFloat(sumAssured);
     preMedical = preMedical.toLowerCase() == 'true' || preMedical.toLowerCase() == 'yes' ? true : false
     medical = medical.toLowerCase() == 'true' || medical.toLowerCase() == 'yes' ? true : false
     travelParty = travelParty == '' ? null : travelParty;
